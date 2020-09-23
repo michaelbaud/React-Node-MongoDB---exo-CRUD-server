9ca15a9ebef763d5d70b941e7b90779c74614717
Mika77290 <Michael.baudoin.7@gmail.com>
2020-09-23 23:49:37 +0200

    set cors

--- index.js
+++ index.js
@@ -23,6 +23,11 @@ db.once('open', () => {
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
 
 app.use("/api/argonaute", argonauteRoutes)
 
